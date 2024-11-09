336198543781df1eba9fa61b279192c9c02d1415
Tolandorado <tonisagaz@mail.ru>
2024-11-09 19:57:18 +0300

    Added task controllers/routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 /* Route imp */
 import projectRoutes from "./routes/projectRoutes";
+import taskRoutes from "./routes/taskRoutes";
 
 /* Config */
 dotenv.config();
@@ -24,6 +25,7 @@ app.get('/', (req, res) => {
 })
 
 app.use("/projects", projectRoutes);
+app.use("/tasks", taskRoutes);
 
 
 /* Server */
